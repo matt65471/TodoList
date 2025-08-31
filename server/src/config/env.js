61edfc528b98b19db7358911837f7538138f5c17
matt65471 <matt65471@gmail.com>
2025-08-31 15:43:28 -0500

    Added files for backend

---
+++ server/src/config/env.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+
+export const env = {
+  node: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 5000,
+  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/merndb',
+  corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:5173'
+};
---
+++ server/src/db/connect.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+async function connect(uri) {
+  mongoose.set('strictQuery', true);
+  await mongoose.connect(uri);
+  console.log('Mongo connected');
+}
+
+module.exports = { connect };
