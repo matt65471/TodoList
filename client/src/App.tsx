918d2c52e972d620df345d0810178c3730ab2098
matt65471 <matt65471@gmail.com>
2025-09-02 21:33:26 -0500

    Added controller

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,9 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+          <p>
+            
+          </p>
         </a>
       </div>
       <h1>Vite + React</h1>
