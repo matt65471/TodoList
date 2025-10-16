82e95f3354e6edf0f8558abcfa3051e1ae5ee80e
matt65471 <matt65471@gmail.com>
2025-10-16 16:38:20 -0500

    Added axios

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -4,5 +4,5 @@ export const env = {
   node: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 5000,
   mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/merndb',
-  corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:5173'
+  corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:5174'
 };
